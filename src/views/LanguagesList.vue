7001ddcdd04fdd7dd888ef4ff83b92647d2647dd
daisy <daisy@metallurgietech.com>
2023-02-09 20:28:13 +0800

    second

--- src/views/LanguagesList.vue
+++ src/views/LanguagesList.vue
@@ -16,25 +16,44 @@
             </button>
           </div>
         </div>
-        <div class="listContent mx-auto ml-2 max-w-7xl grid grid-cols-12 gap-7 px-8">
-          <div class="col-span-4 ">
-            <div class="sticky top-6 divide-y divide-gray-300">
-              <!-- Your content -->
-              <ul class="border-dashed border-2 border-gray-300 rounded-2xl py-2 px-3 min-h-70percent">
-                <li>aaa</li>
-                <li>bbb</li>
-                <li>ccc</li>
-              </ul>
+        <div class="listContent mx-auto ml-2  grid grid-cols-12 gap-4 px-8">
+          <div class="">
+
+            <div class="mt-8 flex flex-col">
+              <div class="-my-2 -mx-4 sm:-mx-6 lg:-mx-8">
+                <div class="inline-block min-w-full py-2 align-middle">
+                  <div class="shadow-sm ring-1 ring-black ring-opacity-5">
+                    <table class="min-w-full border-separate" style="border-spacing: 0">
+                      <thead class="bg-gray-50">
+                        <tr>
+                          <th scope="col" class="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8">Name</th>
+                          <th scope="col" class="sticky top-0 z-10 hidden border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:table-cell">Title</th>
+                          <th scope="col" class="sticky top-0 z-10 hidden border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter lg:table-cell">Email</th>
+                          <th scope="col" class="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter">Role</th>
+                          <th scope="col" class="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 py-3.5 pr-4 pl-3 backdrop-blur backdrop-filter sm:pr-6 lg:pr-8">
+                            <span class="sr-only">Edit</span>
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody class="bg-white">
+                        <tr v-for="(person, personIdx) in people" :key="person.email">
+                          <td :class="[personIdx !== people.length - 1 ? 'border-b border-gray-200' : '', 'whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8']">{{ person.name }}</td>
+                          <td :class="[personIdx !== people.length - 1 ? 'border-b border-gray-200' : '', 'whitespace-nowrap px-3 py-4 text-sm text-gray-500 hidden sm:table-cell']">{{ person.title }}</td>
+                          <td :class="[personIdx !== people.length - 1 ? 'border-b border-gray-200' : '', 'whitespace-nowrap px-3 py-4 text-sm text-gray-500 hidden lg:table-cell']">{{ person.email }}</td>
+                          <td :class="[personIdx !== people.length - 1 ? 'border-b border-gray-200' : '', 'whitespace-nowrap px-3 py-4 text-sm text-gray-500']">{{ person.role }}</td>
+                          <td :class="[personIdx !== people.length - 1 ? 'border-b border-gray-200' : '', 'relative whitespace-nowrap py-4 pr-4 pl-3 text-right text-sm font-medium sm:pr-6 lg:pr-8']">
+                            <a href="#" class="text-indigo-600 hover:text-indigo-900"
+                              >Edit<span class="sr-only">, {{ person.name }}</span></a
+                            >
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-          <div class="col-span-8 ">
-            <!-- Your content -->
-            <ul class="border-dashed border-2 border-gray-300 rounded-2xl  py-2 px-3 min-h-70percent">
-                <li>aaa</li>
-                <li>bbb</li>
-                <li>ccc</li>
-              </ul>
-          </div>
+  </div>
         </div>
         <KeyIn/>
       </div>
@@ -67,6 +86,11 @@ const languagesList = reactive([
   { id: 11, name: '印尼文' },
   { id: 12, name: '泰文' },
 ])
+
+const people = [
+  { name: 'Lindsay Walton', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
+  // More people...
+]
 
 
 </script>
